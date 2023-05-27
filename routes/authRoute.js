4740447bb580ad8a09e1a8efcc8006b6831de381
Rahul Kumar <rahulraxstar4@gmail.com>
2023-05-27 19:40:00 +0530

    Forgot passoword is implemented

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,24 +1,32 @@
-import express from 'express';
-import {registerController, loginController, testController} from "../controllers/authController.js";
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 // router object
 const router = express.Router();
 
-
 // routing
 // Register || method post
 
-router.post('/register', registerController);
-
+router.post("/register", registerController);
 
 // login // method post
-router.post('/login', loginController);
-
+router.post("/login", loginController);
 
 // test route
+router.post("/forgot-password", forgotPasswordController);
 
-router.get('/test', requireSignIn, isAdmin,testController);
+router.get("/test", requireSignIn, isAdmin, testController);
 
+// protected route auth
+
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 export default router;
